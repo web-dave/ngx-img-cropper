9ab3c26ce3828d9f47ad1d56e4f8ad07725047e5
Guillaume RODRIGUEZ <guiguidu31300@gmail.com>
2018-05-07 18:12:53 +0200

    Angular 6 compatibility

--- runtime/app.module.ts
+++ runtime/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-//import {ImageCropperComponent, CropperSettings, Bounds} from '../index';
-import {ImageCropperComponent} from '../src/imageCropperComponent';
-import {CropperSettings} from '../src/cropperSettings';
-import {Bounds} from '../src/model/bounds';
+import { ImageCropperComponent } from '../src/imageCropperComponent';
+import { CropperSettings } from '../src/cropperSettings';
+import { Bounds } from '../src/model/bounds';
 
 import { HttpModule } from '@angular/http';
-import 'rxjs/Rx';
 
 import { AppComponent } from './app';
-import {TabsModule} from 'ng2-tabs';
+import { TabsModule } from 'ngx-tabs';
 
 @NgModule({
     imports: [ BrowserModule, TabsModule, FormsModule ],
