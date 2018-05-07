4c9b72d90101eb450e877bfa82ad108758db3755
Guillaume RODRIGUEZ <guiguidu31300@gmail.com>
2018-05-07 18:09:20 +0200

    Remove useless files

--- src/model/cropService.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class CropService {
-    canvas: HTMLCanvasElement;
-    ctx: CanvasRenderingContext2D;
-    init(canvas: HTMLCanvasElement): void;
-}
--- src/model/cropTouch.d.ts
+++
@@ -1,8 +0,0 @@
-import { IHandle } from "./handle";
-export declare class CropTouch {
-    x: number;
-    y: number;
-    id: number;
-    dragHandle: IHandle;
-    constructor(x?: number, y?: number, id?: number);
-}
