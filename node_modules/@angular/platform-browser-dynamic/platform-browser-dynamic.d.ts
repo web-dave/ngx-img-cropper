134eb7616d31848e4b9098333f368803738e1e2a
Guillaume RODRIGUEZ <guiguidu31300@gmail.com>
2018-05-07 18:05:36 +0200

    Remove node_modules

--- node_modules/@angular/platform-browser-dynamic/platform-browser-dynamic.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { JitCompilerFactory as ɵa } from './src/compiler_factory';
-export { CachedResourceLoader as ɵb } from './src/resource_loader/resource_loader_cache';
