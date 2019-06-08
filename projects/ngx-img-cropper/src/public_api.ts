bf3d2c4e91f60a79d58afa1a330698fbe2e0aa3a
Dave <dave@webdave.de>
2019-06-08 23:19:34 +0200

    export all

--- projects/ngx-img-cropper/src/public_api.ts
+++ projects/ngx-img-cropper/src/public_api.ts
@@ -2,8 +2,12 @@
  * Public API Surface of ngx-img-cropper
  */
 
-export * from './lib/image-cropper/image-cropper.service';
+export * from './lib/image-cropper/cropper-draw-settings';
+export * from './lib/image-cropper/cropper-settings';
+export * from './lib/image-cropper/exif';
 export * from './lib/image-cropper/image-cropper.component';
 export * from './lib/image-cropper/image-cropper.module';
-export * from './lib/image-cropper/cropper-settings';
-export * from './lib/image-cropper/model/cropPosition';
+export * from './lib/image-cropper/image-cropper.service';
+export * from './lib/image-cropper/imageCropper';
+export * from './lib/image-cropper/imageCropperDataShare';
+export * from './lib/image-cropper/model';
