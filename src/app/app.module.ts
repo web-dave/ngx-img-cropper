5e502facf22dae1535754f42b0d2b3bdf83b933e
Polatouche <antoine@jabron.com>
2020-07-23 12:55:20 +0200

    ng update 9 rc4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTabsModule, MatExpansionModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
-// import { ImageCropperModule } from 'projects/ngx-img-cropper/src/public_api';
-import { ImageCropperModule } from 'dist/ngx-img-cropper';
+import { ImageCropperModule } from 'projects/ngx-img-cropper/src/public_api';
+// import { ImageCropperModule } from 'dist/ngx-img-cropper';
 
 @NgModule({
   declarations: [AppComponent],
