75a34e261885c4185f670fb0c9035d911b148bc5
Tom-Rune Bornholdt <trb@braune-digital.com>
2020-08-12 12:44:26 +0200

    Update angular material imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTabsModule, MatExpansionModule } from '@angular/material';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
 // import { ImageCropperModule } from 'projects/ngx-img-cropper/src/public_api';
@@ -16,9 +17,9 @@ import { ImageCropperModule } from 'dist/ngx-img-cropper';
     FormsModule,
     NoopAnimationsModule,
     MatTabsModule,
-    MatExpansionModule
+    MatExpansionModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
