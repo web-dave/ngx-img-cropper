b2acd44a1ee48b701ec4c01db2939aadd346b8be
web-dave <dave@webdave.de>
2022-08-30 21:56:30 +0200

    et löp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { CropPosition } from 'projects/ngx-img-cropper/src/lib/image-cropper/mod
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   // Cropper 1 data
